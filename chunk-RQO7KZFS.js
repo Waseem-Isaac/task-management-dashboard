6f040027428a2bf0091edd949fbe320b4a1070ec
Waseem-Isaac <waseemisac@gmail.com>
2026-05-05 21:39:01 +0000

    Deploying to gh-pages from @ Waseem-Isaac/boardly@fb640c9d7e2e332af5306a8d5140d2c59a57cecb 🚀

--- chunk-RQO7KZFS.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-IONH7NRB.js";var t=[{path:"join",loadComponent:()=>import("./chunk-WP5J2CE7.js").then(o=>o.JoinComponent)},{path:"set-password/:token",loadComponent:()=>import("./chunk-QIOUSPAJ.js").then(o=>o.SetPasswordComponent)},{path:"",redirectTo:"join",pathMatch:"full"},{path:"**",redirectTo:"join"}];export{t as AUTH_ROUTES};
