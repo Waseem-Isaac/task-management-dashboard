750158f73562703958cc9a7c820bbcaf2b6beb82
@wibrahim.eg <waseemisac@gmail.com>
2026-05-08 14:18:40 +0300

    code cleanup

--- frontend/src/app/features/analytics/completion-rate/completion-rate.ts
+++ frontend/src/app/features/analytics/completion-rate/completion-rate.ts
@@ -1,11 +1,13 @@
 import { Component, input } from '@angular/core';
 import { MatProgressBar } from '@angular/material/progress-bar';
+import { AnalyticsCardPlaceholder } from "../components/analytics-card-placeholder/analytics-card-placeholder";
 @Component({
   selector: 'app-completion-rate',
-  imports: [ MatProgressBar ],
+  imports: [MatProgressBar, AnalyticsCardPlaceholder],
   templateUrl: './completion-rate.html',
   styleUrl: './completion-rate.scss',
 })
 export class CompletionRate {
   completionRateData = input<{ completionRate:number, doneCount: number, todoCount: number, inProgressCount: number, overdueCount: number }>();
+  totalTasks = input<number>(0);
 }
--- frontend/src/app/features/analytics/tasks-per-member/tasks-per-member.ts
+++ frontend/src/app/features/analytics/tasks-per-member/tasks-per-member.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { MatProgressBar } from "@angular/material/progress-bar";
+import { AnalyticsCardPlaceholder } from '../components/analytics-card-placeholder/analytics-card-placeholder';
 
 @Component({
   selector: 'app-tasks-per-member',
-  imports: [MatProgressBar],
+  imports: [MatProgressBar , AnalyticsCardPlaceholder],
   templateUrl: './tasks-per-member.html',
   styleUrl: './tasks-per-member.scss',
 })
