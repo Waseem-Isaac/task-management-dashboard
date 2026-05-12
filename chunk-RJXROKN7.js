a20f765a1cb54a713ad6955752149d92337a8486
Waseem-Isaac <waseemisac@gmail.com>
2026-05-12 19:43:01 +0000

    Deploying to gh-pages from @ Waseem-Isaac/boardly@70d1a24977c2c5be6278c0284c2de910b996770a 🚀

--- chunk-RJXROKN7.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-LG6SK6BE.js";var t=[{path:"join",loadComponent:()=>import("./chunk-VEWRDR5H.js").then(o=>o.JoinComponent)},{path:"set-password/:token",loadComponent:()=>import("./chunk-BW5MNTJ5.js").then(o=>o.SetPasswordComponent)},{path:"",redirectTo:"join",pathMatch:"full"},{path:"**",redirectTo:"join"}];export{t as AUTH_ROUTES};
